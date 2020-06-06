73cf2fcf57e88224d006b480d286edd05936cb16
denniz-10 <denilson_medrano@hotmail.com>
2020-06-06 19:33:14 -0300

    adicionando orbita aos elementos renderizados

--- js/function.js
+++ js/function.js
@@ -2,20 +2,28 @@ var scene;
 var camera;
 var renderer;
 var sphere;
+var controls;
+var usjt;
 
 var init = () => {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
-    45,
+    50,
     window.innerWidth / window.innerHeight,
     1,
-    1000
+    10000
   );
 
-  renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  var gridXZ = new THREE.GridHelper(100, 10);
+  gridXZ.setColors(new THREE.Color(0xff0000), new THREE.Color(0xffffff));
+  scene.add(gridXZ);
+
+  controls.update();
   this.createAEsphere();
 
   camera.position.x = 25;
@@ -33,8 +41,8 @@ var render = () => {
 };
 
 var animate = () => {
-  // sphere.rotation.x -= 0.01;
   sphere.rotation.y += 0.01;
+  usjt.rotation.y -= 0.03;
 };
 
 var createAEsphere = () => {
@@ -42,7 +50,16 @@ var createAEsphere = () => {
   let material = this.createMaterial();
   sphere = new THREE.Mesh(geometry, material);
   sphere.name - "mySphere";
+  sphere.position.set(0, 12, 0);
+
+  let Usjtgeometry = new THREE.SphereGeometry(7, 32, 32);
+  let Usjtmaterial = this.createMaterialUsjt();
+  usjt = new THREE.Mesh(Usjtgeometry, Usjtmaterial);
+  usjt.name - "SãoJudas";
+  usjt.position.set(30, 12, 0);
+
   scene.add(sphere);
+  scene.add(usjt);
 };
 
 var createMaterial = () => {
@@ -51,6 +68,13 @@ var createMaterial = () => {
   );
   let sphereMaterial = new THREE.MeshBasicMaterial({ map: sphereTexture });
   return sphereMaterial;
+};
+var createMaterialUsjt = () => {
+  let sphereTexture = new THREE.TextureLoader().load(
+    "assets/images/usjt_logo.png"
+  );
+  let sphereMaterialUsjt = new THREE.MeshBasicMaterial({ map: sphereTexture });
+  return sphereMaterialUsjt;
 };
 
 window.onload = this.init;
